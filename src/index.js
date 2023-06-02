c4ac81f9d855678fa80ecf9fed356427f6db5e4d
dristosun <107073369+dristosun@users.noreply.github.com>
2023-06-02 21:44:45 +0300

    Functionality to open and close modals

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import './styles.css';
 
-const modal = document.querySelector('.modal-container');
-const addDep = document.querySelector('.addDep')
+const modals = document.querySelectorAll('.modal-container');
+const newDepModal = document.querySelector('.mod-contNewDep');
+const newTaskModal = document.querySelector('.mod-contNewTask');
+const colorMapModal = document.querySelector('.mod-contColorMap');
+const addDep = document.querySelector('.addDep');
+const colorMap = document.querySelector('.colorMap')
 
 const DOM = {
-    
+    openModal(modal) {
+        modal.style.display = 'flex';
+    },
+    closeModal(modal) {
+        modal.style.display = 'none';
+    }
 }
+addDep.addEventListener('click', function() {
+    DOM.openModal(newDepModal);
+});
+colorMap.addEventListener('click', function() {
+    DOM.openModal(colorMapModal);
+});
 
+window.onclick = function(e) {
+    modals.forEach(modal => {
+        if (modal === e.target)
+            DOM.closeModal(modal);
+    })
+}
